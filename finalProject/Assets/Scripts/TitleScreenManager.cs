8c1ca65fe92679c6dd15a5af4aedcef6fdb193a2
dspielmann <diego.spielmann@gmail.com>
2025-10-26 19:40:33 -0400

    Created start screen
    - added start screen
    - added player selection screen
    - added ui and buttons to start screen
    - coded buttons to quit and move to player selection

---
+++ finalProject/Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;  // Important for Button references
+
+public class TitleScreenManager : MonoBehaviour
+{
+    private Button playButton;
+    private Button quitButton;
+
+    void Awake()
+    {
+        // Find buttons by name in the scene
+        playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
+
+        // Add listeners in code
+        playButton.onClick.AddListener(PlayGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("Play button clicked!");
+        SceneManager.LoadScene("PlayerSelection");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit button clicked!");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
